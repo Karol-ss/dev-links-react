0e9e24548dd434b5b2f9b5931e11d30c2c9f2cb8
Karol-ss <KarolSENAI1234@gmail.com>
2023-08-11 10:20:45 -0300

    desenvolvido profile

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,21 @@
-import "./App.css";
+import { useState } from "react";
 import Profile from "../components/profile/profile";
 
-export default function App() {
-    return(
-        <>
-      <Profile></Profile>
-        </>
-    )
-};
+import "./App.css";
+
+import imgDarkMode from "../img/fotoPerfil.jpg"
+import imgLightMode from "../img/fotoPerfilClaro.png"
+
+const App = () => {
+const [isLightMode,setIsLightMode] = useState(true)
+
+const imgSrc = isLightMode ? imgLightMode : imgDarkMode
+const imgAlt = isLightMode ? "Imagem no Modo Claro" : "Imagem no Modo Escuro"
+  return(
+    <div>
+      <Profile imgSrc={imgSrc} imgAlt={imgAlt}>@Karol-ss</Profile>
+    </div>
+  )
+}
+
+export default App;
